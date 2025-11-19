0ab9bce326a9f90167c32fe1e0c3308072a852eb
lenglengiOS <18708363472@qq.com>
2025-11-19 18:56:46 +0800

    集成react

--- main.js
+++ main.js
@@ -1,26 +1,13 @@
-const { app, BrowserWindow } = require("electron/main");
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
 
-const createWindow = () => {
+function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
   });
 
-  win.loadFile("index.html");
-};
+  win.loadFile(path.join(__dirname, "src/renderer/index.html"));
+}
 
-app.whenReady().then(() => {
-  createWindow();
-
-  app.on("activate", () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
-    }
-  });
-});
-
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
-});
+app.whenReady().then(createWindow);
---
+++ webpack.renderer.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/renderer/index.jsx",
+  output: {
+    path: path.resolve(__dirname, "src/renderer"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+};
