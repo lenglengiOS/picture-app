c2769501bec212d24c6fb5b8249b75b1ac61bce2
lenglengiOS <18708363472@qq.com>
2025-11-19 22:36:56 +0800

    配置打包和icon

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: path.join(__dirname, "build/icon.png"),
   });
 
   win.loadFile(path.join(__dirname, "src/renderer/index.html"));
