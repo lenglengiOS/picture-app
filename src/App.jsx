1dd302ce937e46966aea8be0ac0324fee9195af9
lenglengiOS <18708363472@qq.com>
2025-11-20 10:56:14 +0800

    完成图标和路径别名配置

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import Button from "@material-ui/core/Button";
+
+const App = () => {
+  return <Button variant="contained">你好，世界66</Button>;
+};
+
+export default App;
--- src/renderer/index.jsx
+++ src/renderer/index.jsx
@@ -1,8 +1,5 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-
-function App() {
-  return <h1>Hello from React + Electron!888999</h1>;
-}
+import App from "@src/App";
 
 createRoot(document.getElementById("root")).render(<App />);
